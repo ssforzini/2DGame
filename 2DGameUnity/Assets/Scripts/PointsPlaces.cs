0b19d51686668e00e5486784517ebf598bf58a17
ssforzini <sebasforzini@hotmail.com>
2017-06-26 00:06:24 -0300

    Cambios en los scripts, para la toma del puntaje

---
+++ 2DGameUnity/Assets/Scripts/PointsPlaces.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PointsPlaces : MonoBehaviour {
+
+	[SerializeField]private int point;
+	private int score = 0;
+
+	void Start(){
+		if (SceneManager.GetActiveScene().name == "FirstLevel") {
+			
+		} else {
+			score = PlayerPrefs.GetInt ("HiScore");
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void OnCollisionEnter2D(Collision2D col){
+		if(col.gameObject.name == "Ship"){
+			score += point;
+			Debug.Log (score);
+
+			PlayerPrefs.SetInt("HiScore",score);
+			Scene scene = SceneManager.GetActiveScene();
+			SceneManager.LoadScene(scene.name);
+		}
+	}
+		
+}
